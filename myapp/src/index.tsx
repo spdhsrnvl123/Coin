0a092ea711742f03755db349f655f5dc1f74ac56
Leutbing <spdhsrnvl123@naver.com>
2022-07-27 15:03:58 +0900

    20220727

---
+++ myapp/src/index.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import { ThemeProvider } from "styled-components";
+import { theme } from "./Theme";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+
+const queryClient = new QueryClient();
+
+const root = ReactDOM.createRoot(
+  document.getElementById("root") as HTMLElement
+);
+
+root.render(
+  //   <React.StrictMode>
+  <QueryClientProvider client={queryClient}>
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
+  </QueryClientProvider>
+
+  //   </React.StrictMode>
+);
