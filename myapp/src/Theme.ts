7a284102f400bcca224d0ddaf7f780164be30102
Leutbing <spdhsrnvl123@naver.com>
2022-08-10 15:26:35 +0900

    update

--- myapp/src/Theme.ts
+++ myapp/src/Theme.ts
@@ -1,7 +1,7 @@
 import { DefaultTheme } from "styled-components";
 
 export const theme: DefaultTheme = {
-  bgColor: "#2f3640",
-  textColor: "#f5f6fa",
+  bgColor: "#2f3640", //진한 회색
+  textColor: "#f5f6fa", //흰색
   accentColor: "skyblue",
 };
